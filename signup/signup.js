1c6823415d13975508c5b5a92f174f077ca27bea
Aryaa-Prangya <138790828+Aryaa-Prangya@users.noreply.github.com>
2023-07-09 15:49:34 +0530

    Add files via upload

---
+++ signup/signup.js
@@ -0,0 +1,24 @@
+const email = document.getElementById("email");
+const uname = document.getElementById("name");
+const pass = document.getElementById("password");
+const button = document.getElementById("signup");
+
+button.addEventListener("click", () => {
+  const dataobj = {
+    email: email.value,
+    name: uname.value,
+    password: pass.value,
+  };
+  fetch("http://localhost:4046/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataobj),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert("Account created successfully!");
+      window.location.href = "../signin/signin.html";
+    });
+});
